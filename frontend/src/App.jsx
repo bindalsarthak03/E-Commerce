46a270480041c9129c23a1a034e785fe42d1c495
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2023-02-22 11:09:22 +0530

    Feature Products added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css';
+import data from './data';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">E-Commerce</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className='products'>
+          {
+            data.products.map(product => (
+              <div className='product' key={product.slug}>
+                <a href={`product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className='product-info'>
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p><strong>₹{product.price}</strong></p>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            )
+            )
+          }
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
