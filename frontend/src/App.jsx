0ebbd18aaabd02ab3e4000599f98466aa43825d0
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2023-02-22 19:02:54 +0530

    styles changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,33 @@ import './App.css';
 import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import {LinkContainer} from 'react-router-bootstrap';
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to="/">E-Commerce</Link>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>E-Commerce</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/" element={<HomeScreen/>}></Route>
           <Route path="/product/:slug" element={<ProductScreen/>}></Route>
         </Routes>
+        </Container>
       </main>
+      <footer>
+        <div className='text-center'>All rights reserved</div>
+      </footer>
     </div>
     </BrowserRouter>
   );
