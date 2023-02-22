22e7935dfefdb8dec01bbf78adc668af8389034c
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2023-02-22 11:34:33 +0530

    Added routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,22 @@
-
 import './App.css';
-import data from './data';
-
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <header>
-        <a href="/">E-Commerce</a>
+        <Link to="/">E-Commerce</Link>
       </header>
       <main>
-        <h1>Featured Products</h1>
-        <div className='products'>
-          {
-            data.products.map(product => (
-              <div className='product' key={product.slug}>
-                <a href={`product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className='product-info'>
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p><strong>₹{product.price}</strong></p>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            )
-            )
-          }
-        </div>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>}></Route>
+          <Route path="/product/:slug" element={<ProductScreen/>}></Route>
+        </Routes>
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,12 @@
+import data from "../data"
+import {useParams} from 'react-router-dom'
+function ProductScreen(){
+    const params = useParams();
+    const {slug} = params
+    return (
+        <div>
+            <h1>{slug}</h1>
+        </div>
+    )
+}
+export default ProductScreen
